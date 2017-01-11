92599c8f931b0b8f0992371af37a1238544d7cdd
Andrew Daly <andrew.daly@students.ittralee.ie>
2017-01-11 17:11:00 +0000

    Added remove numbers and strp tags rule.

--- test.js
+++ test.js
@@ -2,6 +2,10 @@ function dropList() {
     document.getElementById("drop_list").classList.toggle("show");
 }
 
+function dropList2() {
+    document.getElementById("drop_list2").classList.toggle("show");
+}
+
 window.onclick = function(event) {
     if (!event.target.matches('.drop_btn')) {
 
@@ -24,8 +28,8 @@ function alertResult(rule) {
 
 function fieldAppend() {
     var append = "Append[[-SEP-]]FORMID[[-SEP-]]DFIELD[[-SEP-]]LVALUE[[-SEP-]]APPND[[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]]";
-    var formID = prompt("Enter Form ID:");
-    var dField = prompt("Enter Destination Field:");
+    var formID = prompt("Enter form ID:");
+    var dField = prompt("Enter destination Field:");
     var lValue = prompt("Enter field containing live value:");
     var sAppend = prompt("Enter string to append:");
 
@@ -36,8 +40,8 @@ function fieldAppend() {
 
 function fieldPrepend() {
     var prepend = "Prepend[[-SEP-]]FORMID[[-SEP-]]DFIELD[[-SEP-]]LVALUE[[-SEP-]]PPPND[[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]]";
-    var formID = prompt("Enter Form ID:");
-    var dField = prompt("Enter Destination Field:");
+    var formID = prompt("Enter form ID:");
+    var dField = prompt("Enter destination Field:");
     var lValue = prompt("Enter field containing live value:");
     var sPrepend = prompt("Enter string to append:");
 
@@ -48,8 +52,8 @@ function fieldPrepend() {
 
 function removeNumbers() {
     var remove = "RemoveNumbers[[-SEP-]]FORMID[[-SEP-]]DFIELD[[-SEP-]]LVALUE[[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]]";
-    var formID = prompt("Enter Form ID:");
-    var dField = prompt("Enter Destination Field:");
+    var formID = prompt("Enter form ID:");
+    var dField = prompt("Enter destination Field:");
     var lValue = prompt("Enter field containing live value:");
 
     var myRule = remove.replace(/FORMID/g, formID).replace(/DFIELD/g, dField).replace(/LVALUE/g, lValue);
@@ -58,8 +62,8 @@ function removeNumbers() {
 
 function stripTags() {
     var strip = "StripTags[[-SEP-]]FORMID[[-SEP-]]DFIELD[[-SEP-]][[-SEP-]]LVALUE[[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]]";
-    var formID = prompt("Enter Form ID:");
-    var dField = prompt("Enter Destination Field:");
+    var formID = prompt("Enter form ID:");
+    var dField = prompt("Enter destination Field:");
     var lValue = prompt("Enter field containing live value:");
 
     var myRule = strip.replace(/FORMID/g, formID).replace(/DFIELD/g, dField).replace(/LVALUE/g, lValue);
@@ -67,6 +71,48 @@ function stripTags() {
 }
 
 function concat() {
+    var concat = "Concat[[-SEP-]]FORMID[[-SEP-]]DFIELD[[-SEP-]]876[[-SEP-]]strCDA_876_field_0_54[[-SEP-]][[-SEP-]]876[[-SEP-]][[-SEP-]]#bxp_Space#[[-SEP-]]876[[-SEP-]]strCDA_876_field_0_8[[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]]";
+    var formID = prompt("Enter form ID of destination field:");
+    var dField = prompt("Enter destination Field:");
+
+}
+
+function readFrom() {
+    var readFrm = "ReadFrom[[-SEP-]]FORMID[[-SEP-]]DFIELD[[-SEP-]]FORMID2[[-SEP-]]FieldToSearch[[-SEP-]]ValueToSearchFor[[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]]";
+    var formID = prompt("Enter form ID of destination field:");
+    var dField = prompt("Enter destination Field:");
+    var formID2 = prompt("Enter form ID of field to search");
+    var ftSearch = prompt("Enter the field to search: ");
+    var vtSearch = prompt("Enter the value to search: ");
+
+    var myRule = readFrm.replace(/FORMID/g, formID).replace(/DFIELD/g, dField).replace(/FORMID2/g, formID2).replace(/FieldToSearch/g, ftSearch).replace(/ValueToSearchFor/g, vtSearch);
+    alertResult(myRule);
+}
+
+function readUntil() {
+    var readFrm = "ReadUntil[[-SEP-]]FORMID[[-SEP-]]DFIELD[[-SEP-]]FORMID2[[-SEP-]]FieldToSearch[[-SEP-]]ValueToSearchFor[[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]]";
+    var formID = prompt("Enter form ID of destination field:");
+    var dField = prompt("Enter destination Field:");
+    var formID2 = prompt("Enter form ID of field to search");
+    var ftSearch = prompt("Enter the field to search: ");
+    var vtSearch = prompt("Enter the value to search: ");
+
+    var myRule = readFrm.replace(/FORMID/g, formID).replace(/DFIELD/g, dField).replace(/FORMID2/g, formID2).replace(/FieldToSearch/g, ftSearch).replace(/ValueToSearchFor/g, vtSearch);
+    alertResult(myRule);
+}
+
+function setValue() {
+    var setVal = "SetValue[[-SEP-]]FORMID[[-SEP-]]DFIELD[[-SEP-]]FORMID[[-SEP-]][[-SEP-]]VALUE[[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]]";
+    var formID = prompt("Enter form ID of destination field:");
+    var dField = prompt("Enter destination Field:");
+    var value = prompt("Enter value");
+
+    var myRule = setVal.replace(/FORMID/g, formID).replace(/DFIELD/g, dField).replace(/VALUE/g, value);
+    alertResult(myRule)
+}
+
+function addition() {
+    var additionRule = "AddRule Name[[-SEP-]]Form Id[[-SEP-]]Destination field[[-SEP-]]First Number field[[-SEP-]]Second Number field[[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]]";
 
 }
 
